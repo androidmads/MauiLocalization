0f537f6aa1bee589b7ca85caa79a52226687c50a
Mushtaq M A <mushtaqat3gb@gmail.com>
2023-06-11 13:24:54 +0530

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace MauiLocalization;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+        if (LangResources.Culture != null && LangResources.Culture.TwoLetterISOLanguageName.Contains("ta"))
+        {
+            CounterBtn.Text = "Change to English";
+        }
+        else
+        {
+            CounterBtn.Text = "Change to Tamil";
+        }
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        if (CounterBtn.Text.Contains("Tamil"))
+        {
+            LangResources.Culture = new CultureInfo("ta");
+            CounterBtn.Text = "Change to English";
+        }
+        else
+        {
+            LangResources.Culture = CultureInfo.InvariantCulture;
+            CounterBtn.Text = "Change to Tamil";
+        }
+        App.Current.MainPage = new MainPage();
+    }
+}
+
